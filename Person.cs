01858cffefbf035f33e32b040a970259484e8d11
Ashwini <salunkheashwini1998@gmail.com>
2023-01-26 19:36:39 +0530

    model class is updated

--- Person.cs
+++ Person.cs
@@ -8,25 +8,24 @@ namespace CollectionAddressBook
 {
     public class Person
     {
-        public string FirstName;
-        public string LastName;
-        public string PhoneNumber;
-        public string Address;
-        public string City;
-        public string State;
-        public string ZipCode;
-        public string PhoneNum;
-        public string EmailId;
-        public Person(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNum, string EmailId)
+        public string[] Array_of_Details = new string[8];
+        public Person(string First, string Last, string Add, string City, string State, string Zip, string Phone, string Email)
         {
-            this.FirstName = FirstName;
-            this.LastName = LastName;
-            this.Address = Address;
-            this.City = City;
-            this.State = State;
-            this.ZipCode = ZipCode;
-            this.PhoneNum = PhoneNum;
-            this.EmailId = EmailId;
+            Array_of_Details[0] = First;
+            Array_of_Details[1] = Last;
+            Array_of_Details[2] = Add;
+            Array_of_Details[3] = City;
+            Array_of_Details[4] = State;
+            Array_of_Details[5] = Zip;
+            Array_of_Details[6] = Phone;
+            Array_of_Details[7] = Email;
+        }
+        public void Check()
+        {
+            Console.WriteLine("\nThe details for {0} {1} are:\nAddress: {2}\nCity: {3}\nState: {4}\n" +
+                "Zip Code: {5}\nPhone Number: {6}\nEmail: {7}\n", Array_of_Details[0],
+                Array_of_Details[1], Array_of_Details[2], Array_of_Details[3],
+                Array_of_Details[4], Array_of_Details[5], Array_of_Details[6], Array_of_Details[7]);
         }
 
     }
